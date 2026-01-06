9564ebedc3fda4a6bf06d3ddf47229e002d1fe7a
Mateus Gonçalves <mateusenrique123@gmail.com>
2026-01-06 08:53:54 -0300

    a

--- Microsservico/cliente-service/src/main/java/br/com/exemplo/cliente/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.exemplo.cliente.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.exemplo.cliente.entity.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
