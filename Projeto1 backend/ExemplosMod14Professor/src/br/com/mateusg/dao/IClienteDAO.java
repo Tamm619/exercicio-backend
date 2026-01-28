134999c5565d330ae532b7826a16a8bca6e25d17
Mateus Gonçalves <mateusenrique123@gmail.com>
2026-01-28 14:56:56 -0300

    Tarefa Projeto1 retomada

---
+++ Projeto1 backend/ExemplosMod14Professor/src/br/com/mateusg/dao/IClienteDAO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.mateusg.dao;
+
+import br.com.mateusg.domain.Cliente;
+
+;import java.util.Collection;
+
+/**
+ *
+ * @author mateus.goncalves
+ */
+public interface IClienteDAO {
+    
+    public Boolean cadastrar(Cliente cliente);
+    
+    public void excluir(Long cpf);
+    
+    public void alterar(Cliente cliente);
+    
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
